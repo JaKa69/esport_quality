ef9d8a805bd572d8df5cc2d788cdec900ead5098
Alexis EVENO <eveno.alexis@orange.fr>
2025-04-22 14:36:41 +0200

    bdd h2 pour les tests

--- src/test/java/com/example/esport/EsportApplicationTests.java
+++ src/test/java/com/example/esport/EsportApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.esport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class EsportApplicationTests {
+@ActiveProfiles("test")
+public class EsportApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// test simple pour valider le contexte Spring
 	}
-
 }
