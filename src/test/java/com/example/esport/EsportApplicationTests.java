492bfe5d7ad8753ed027c55d6b8f9591ad28447c
Alexis EVENO <135007874+EVNALEXIS@users.noreply.github.com>
2025-04-22 16:38:56 +0200

    Update EsportApplicationTests.java

--- src/test/java/com/example/esport/EsportApplicationTests.java
+++ src/test/java/com/example/esport/EsportApplicationTests.java
@@ -3,13 +3,15 @@ package com.example.esport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class EsportApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// test simple pour valider le contexte Spring
-	}
+    @Test
+    void contextLoads() {
+        // Test pour valider le chargement du contexte Spring
+    }
 }
